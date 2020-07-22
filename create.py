da3530d4faa354989e3142c950723c6d9f7221dc
Yubei Yang <58965389+ConyYang@users.noreply.github.com>
2020-07-22 17:32:31 +0800

    Add files via upload
    cal_ZCR(self):
    cal_spectral_centroid(self):
    cal_spectral_rolloff(self):

---
+++ create.py
@@ -0,0 +1,7 @@
+import numpy as np
+import librosa
+T= 5.0
+sr = 22050
+t = np.linspace(start=0.0, stop=T, num=int(T*sr), endpoint=False)
+x = 0.5 * np.sin(2*np.pi*220*t)
+librosa.output.write_wav('tone_220.wav',y=x, sr=sr)
--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import librosa
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 import librosa.display
+import sklearn
 
 audio_path = 'beats.wav'
 # ipd.Audio(audio_path)
@@ -33,5 +34,42 @@ class Music(object):
         plt.savefig('spectrum.jpg')
         plt.show()
 
+    def cal_ZCR(self):
+        n0 = 39000
+        n1 = 39100
+        plt.figure(figsize=(20, 5))
+        plt.plot(self.x[n0: n1])
+        plt.grid()
+        plt.savefig('zcrPortion.png')
+        plt.show()
+        zero_crossings = librosa.zero_crossings(y=self.x[n0:n1],pad=False)
+        return sum(zero_crossings)
+
+
+    def cal_spectral_centroid(self):
+        cent = librosa.feature.spectral_centroid(y=self.x, sr=self.sr)
+        # print(cent[0].shape)
+        # compute time variable for visualization
+        frames = range(len(cent[0]))
+        t = librosa.frames_to_time(frames)
+        normalized_cent = sklearn.preprocessing.minmax_scale(cent[0], axis=0)
+        plt.figure(figsize=(20, 5))
+        librosa.display.waveplot(y=self.x, sr=self.sr, alpha=0.4)
+        plt.plot(t, normalized_cent, color='red')
+        # plt.savefig('spectralCentroids.png')
+        plt.show()
+
+    def cal_spectral_rolloff(self):
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=self.x+0.01, sr=self.sr)[0]
+        print(spectral_rolloff.shape)
+        normalized_rolloff = sklearn.preprocessing.minmax_scale(spectral_rolloff, axis=0)
+        frames = range(len(spectral_rolloff))
+        t = librosa.frames_to_time(frames)
+        plt.figure(figsize=(20, 5))
+        librosa.display.waveplot(y=self.x, sr=self.sr, alpha=0.4)
+        plt.plot(t, normalized_rolloff, color = "red")
+        plt.savefig('spectralRollOff.png')
+        plt.show()
+
 myaudio = Music(path=audio_path, x=x, sr=sr)
-myaudio.draw_spectrum()
+myaudio.cal_spectral_rolloff()
